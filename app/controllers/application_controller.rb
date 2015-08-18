c70a704929b33dbdd631cc181d4e6a273e361a73
Bill Curtis <wcurtis@sweettoothhq.com>
2015-08-18 15:41:57 -0400

    Add error json object for 404s

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::API
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_error
+
+  def record_not_found_error
+    error = {
+      error: {
+        message: "This is not the resource you're looking for *jedi hand wave*"
+      }
+    }
+    render json: error, status: :not_found
+  end
+
+  # Returns pagination info for collections
   def meta_for_collection collection
     {
       :total_pages    => collection.total_pages,
